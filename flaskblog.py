3a4caacf9370ee92a03b9aa65b79255ebec82f9d
saujanya01 <saujanyatiwari455@gmail.com>
2019-05-25 02:35:43 +0530

    Initial Commit

---
+++ flaskblog.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+
+posts = [
+    {
+        "author":"Saujanya Tiwari",
+        "title":"blog post 1",
+        "content":"First Post Content",
+        "date_posted":"April 20, 2018"
+    },
+    {
+        "author":"XYZ",
+        "title":"blog post 2",
+        "content":"First Post Content",
+        "date_posted":"April 21, 2018"
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template("home.html",post=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__=='__main__':
+    app.run(debug=True)
