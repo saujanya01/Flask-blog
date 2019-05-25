6f3bb9f84a5cfad8596150e8f13d3945231a436c
saujanya01 <saujanyatiwari455@gmail.com>
2019-05-26 01:00:23 +0530

    Still left

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app=Flask(__name__)
@@ -21,17 +21,25 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template("home.html",post=posts,title="HOME")
 
 @app.route("/about")
 def about():
     return render_template("about.html",title="About")
 
-@app.route("/register")
+@app.route("/register",methods=['POST','GET'])
 def register():
     form = RegistrationForm()
-    
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Login', form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
